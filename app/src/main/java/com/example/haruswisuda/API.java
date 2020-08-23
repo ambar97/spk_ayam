a77cc1b0613e9d85a003548f333332da846e356b
Biem <Abiemassyikin@gmail.com>
2020-08-24 00:13:51 +0700

    finish

--- app/src/main/java/com/example/haruswisuda/API.java
+++ app/src/main/java/com/example/haruswisuda/API.java
@@ -1,7 +1,7 @@
 package com.example.haruswisuda;
 
 public class API {
-    public static String api_service = "http://192.168.43.179/api_ayam/serv/";
+    public static String api_service = "http://192.168.43.207/api_ayam/serv/";
 
     public String getApi_service (){
         return api_service;
